d3d13e7b79162ad7b0c7cd49bf962a98624be1a6
yeongno <dldudsh83@gmail.com>
2022-05-16 19:26:50 +0900

    Dynamic 라우팅_2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,18 @@
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 export default function About() {
-  return <div>여기는 About입니다.</div>;
+  const searchParames = useLocation();
+  const searchParam = searchParames.search;
+  // console.log(searchParames.search);
+  // const obj = new URLSearchParams(uselocation1.search);
+  // console.log(obj);
+  // console.log(obj.get("name"));
+  const query = queryString.parse(searchParam);
+  console.log(query.name);
+  return (
+    <div>
+      <h2>여기는 About입니다.</h2>
+      {query.name && <p> name 은 {query.name} 입니다.</p>}
+    </div>
+  );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,11 +2,11 @@ import { useParams } from "react-router-dom";
 
 export default function Profile(props) {
   const { id } = useParams();
-  console.log(id);
+  console.log(props);
   return (
     <div>
       <h2>profile 페이지 입니다.</h2>
-      {id && <p>id는 {id}입니다.</p>}
+      <p>id는 {id}입니다. </p>
     </div>
   );
 }
